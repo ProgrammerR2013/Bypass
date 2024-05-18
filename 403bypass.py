3b074de57f88d9c9f09bd970d8f620b43c495fa0
ProgrammerR2013 <98456890+ProgrammerR2013@users.noreply.github.com>
2024-05-18 15:25:12 +0530

    Update 403bypass.py

--- 403bypass.py
+++ 403bypass.py
@@ -1,1 +1,33 @@
+#!/bin/python
+# Import all the packages (you have to download coloroma, requests, and urllib3)
+import requests
+import sys
+import coloroma import time
+from coloroma import Fore, Style
+import urllib3
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
+# Setup stuff
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+domain = sys.argv[1]
+path = sys.argv[2]
+url = domain + path
+
+# Opening screen 
+print(
+    Style.BRIGHT
+    + Fore.BLUE
+    + """
+    ___    _______  ______    ______            _______  _______  _______  _______  _______  _______ 
+   /   )  (  __   )/ ___  \  (  ___ \ |\     /|(  ____ )(  ___  )(  ____ \(  ____ \(  ____ \(  ____ )
+  / /) |  | (  )  |\/   \  \ | (   ) )( \   / )| (    )|| (   ) || (    \/| (    \/| (    \/| (    )|
+ / (_) (_ | | /   |   ___) / | (__/ /  \ (_) / | (____)|| (___) || (_____ | (_____ | (__    | (____)|
+(____   _)| (/ /) |  (___ (  |  __ (    \   /  |  _____)|  ___  |(_____  )(_____  )|  __)   |     __)
+     ) (  |   / | |      ) \ | (  \ \    ) (   | (      | (   ) |      ) |      ) || (      | (\ (   
+     | |  |  (__) |/\___/  / | )___) )   | |   | )      | )   ( |/\____) |/\____) || (____/\| ) \ \__
+     (_)  (_______)\______/  |/ \___/    \_/   |/       |/     \|\_______)\_______)(_______/|/   \__/
+                                                                                                    
+                                                                        By ProgrammerR_2013                                                                                              
+    """
+
+p
